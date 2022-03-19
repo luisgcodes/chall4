5128669893e0946aaf6ca72a9ced71d25854913b
luisgcodes <lcienfuegos95@gmail.com>
2022-03-19 12:01:51 -0700

    added questions

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// Questions Array
+const questions = [
+    {    
+        question: "Commonly used data types DO Not include:",
+        answer: "3. alerts",
+        option: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    },
+    {
+        question: "The condition in an if / else statement is enclosed with _______.",
+        answer: "3. parenthesis",
+        options: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"]
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ________.",
+        answer: "4. all of the above",
+        options: ["1. numbers and strings", "2. other arrays", "3. booleans",  "4. all of the above"]
+    },
+    {
+        question: "String values must be enclosed within ________ when being assigned to variables.",
+        answer: "3. quotes",
+        options: ["1. commas","2. curly brackets", "3. quotes", "4. parenthesis"]
+    },
+    {
+        question: "A very useful tool used during development and debuggin for printing content to the debugger is:",
+        answer: "4. console log",
+        options: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"]
+    }
+];
